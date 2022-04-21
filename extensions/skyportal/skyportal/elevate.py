ed4eb465713fcc4d3fc7f50a3c2624cda2475c61
Theodlz <theophile.dulaz@gmail.com>
2022-04-21 19:06:58 +0200

    fixed elevate command

--- extensions/skyportal/skyportal/elevate.py
+++ extensions/skyportal/skyportal/elevate.py
@@ -1,14 +1,20 @@
-import pathlib
-import subprocess
-
+from baselayer.app.config import load_config
+from baselayer.app.models import init_db
 from skyportal.model_util import make_super_user
+
+cfg = load_config()
+init_db(**cfg['database'])
 
 def elevate(
     username: str,
 ):
     if username:
-        make_super_user(username)
-        print("Elevated user {} to superuser".format(username))
+        try:
+            user = make_super_user(username)
+            print(f'Elevated {username} to superuser')
+        except Exception as e:
+            print(f'Failed to elevate {username} to superuser: {e}')
+            print('Try again with a different username (this one probably does not exist')
     else:
         print("No username provided")
     
