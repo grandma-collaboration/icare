c6cab4ff22db112298ab7be2a1c2a182770d0c0d
Theophile du Laz <theophile.dulaz@gmail.com>
2022-05-19 18:30:50 +0900

    Changed update command (#3)
    * updated update command
    
    * updated skyportal-fink-client and skyportal submodules

--- launcher/commands/update.py
+++ launcher/commands/update.py
@@ -12,19 +12,18 @@ def update(
     :param repo: Remote repository to pull from
     :param branch: Branch on the remote repository
     """
-    git_pull_command = ["git", "pull"]
-    if repo is not None and branch is not None:
-        git_pull_command.extend([repo, branch])
-    p = subprocess.run(git_pull_command)
-    if p.returncode != 0:
-        raise RuntimeError("Failed to git pull grandma")
 
     # show diff between patched skyportal and skyportal
     if diff():
         # update submodules
-        p = subprocess.run(["git", "checkout", branch], cwd="skyportal")
-        if p.returncode != 0:
-            raise RuntimeError("Failed to update grandma's submodules")
+        git_pull_command = ["git", "pull"]
+        if repo is not None and branch is not None:
+            p = subprocess.run(["git", "checkout", branch], cwd="skyportal")
+            if p.returncode != 0:
+                raise RuntimeError("Failed to update grandma's submodules")
+            p = subprocess.run(git_pull_command, cwd="skyportal")
+            if p.returncode != 0:
+                raise RuntimeError("Failed to git pull grandma")
         return True
     else:
         return False
