f1fc722ff8de583c7f05170666a9277461588adc
Theodlz <theophile.dulaz@gmail.com>
2024-06-24 09:23:37 -0700

    add upper limits, pass current magsys to use when refreshing the data

--- extensions/skyportal/skyportal/handlers/api/fink_photometry.py
+++ extensions/skyportal/skyportal/handlers/api/fink_photometry.py
@@ -16,22 +16,30 @@ bands = {1: 'ztfg', 2: 'ztfr', 3: 'ztfi'}
 class FinkPhotometryHandler(BaseHandler):
     @auth_or_token
     async def post(self, object_id):
+        if not isinstance(object_id, str):
+            return self.error("Invalid object ID")
+        object_id = object_id.strip()
+
+        data = self.get_json()
+        magsys = data.get('magsys', 'ab')
+        if magsys not in ['ab', 'vega']:
+            return self.error('Invalid magsys, must be either ab or vega')
+
         try:
             with self.Session() as session:
-                if not isinstance(object_id, str):
-                    return self.error("Invalid object ID")
-                object_id = object_id.strip()
                 obj = session.scalars(
                     Obj.select(session.user_or_token).where(Obj.id == object_id)
                 ).first()
                 if not obj:
                     return self.error("Object not found")
+
                 r = requests.post(
                     'https://fink-portal.org/api/v1/objects',
-                    json={'objectId': object_id, 'output-format': 'json'},
+                    json={'objectId': object_id, 'output-format': 'json', 'withupperlim': True},
                 )
                 if r.status_code != 200:
                     return self.error("Request to Fink's API failed")
+
                 df_request = pd.read_json(io.BytesIO(r.content))
                 if len(df_request.index) > 0:
                     desired_columns = [
@@ -48,15 +56,14 @@ class FinkPhotometryHandler(BaseHandler):
                         return self.error('Missing expected column')
 
                     stmt = Instrument.select(session.user_or_token).where(
-
-                        Instrument.name == 'CFH12k'
+                        Instrument.name == 'ZTF'
                     )
                     instrument = session.scalars(stmt).first()
                     if not instrument:
                         return self.error(
-                            'No instrument named CFH12k found to post the photometry'
+                            'Could not find any instrument named ZTF, cannot post photometry from Fink'
                         )
-                    instrument_id = instrument.id
+
                     data = {
                         'obj_id': df_request['i:objectId'],
                         'ra': df_request['i:ra'],
@@ -64,19 +71,19 @@ class FinkPhotometryHandler(BaseHandler):
                         'mag': df_request['i:magpsf'],
                         'magerr': df_request['i:sigmapsf'],
                         'limiting_mag': df_request['i:diffmaglim'],
-                        'filter': [bands[band] for band in df_request['i:fid']],
-                        'mjd': [Time(jd, format="jd").mjd for jd in df_request["i:jd"]],
+                        'filter': [bands[int(band)] for band in df_request['i:fid']],
+                        'mjd': [Time(float(jd), format="jd").mjd for jd in df_request["i:jd"]],
                         'magsys': ['ab' for i in range(len(df_request))],
-                        'instrument_id': instrument_id,
+                        'instrument_id': instrument.id,
                         'group_ids': [
-                            g.id for g in self.current_user.accessible_groups
+                            g.id for g in self.current_user.accessible_groups if not g.single_user_group
                         ],
                     }
-
                     add_external_photometry(data, self.associated_user_object)
+
                 self.push_all(
-                    action='skyportal/REFRESH_SOURCE',
-                    payload={'obj_key': obj.internal_key},
+                    action='skyportal/FETCH_SOURCE_PHOTOMETRY',
+                    payload={'obj_id': obj.id, 'magsys': magsys},
                 )
                 return self.success()
         except Exception as e:
--- extensions/skyportal/static/js/ducks/fink_phot.js
+++ extensions/skyportal/static/js/ducks/fink_phot.js
@@ -2,6 +2,8 @@ import * as API from "../API";
 
 const POST_FINK_PHOTOMETRY = "skyportal/POST_FINK_PHOTOMETRY";
 
-export default function postFinkPhot(id) {
-  return API.POST(`/api/sources/${id}/fink`, POST_FINK_PHOTOMETRY);
+export default function postFinkPhot(id, current_magsys) {
+  return API.POST(`/api/sources/${id}/fink`, POST_FINK_PHOTOMETRY, {
+    magsys: current_magsys || "ab",
+  });
 }
