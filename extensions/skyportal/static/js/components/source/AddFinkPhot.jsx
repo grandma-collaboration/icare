f353d55e61e8fc65c5e4102244453cd42540dee4
Theophile du Laz <theophile.dulaz@gmail.com>
2024-06-28 07:17:03 -0700

    Add photometry from Fink: upper limits and frontend refresh (#92)
    * add upper limits, pass current magsys to use when refreshing the data
    
    * update photometry refresh action name
    
    * change variable name to be more explicit

--- extensions/skyportal/static/js/components/source/AddFinkPhot.jsx
+++ extensions/skyportal/static/js/components/source/AddFinkPhot.jsx
@@ -9,13 +9,22 @@ import postFinkPhot from "../../ducks/fink_phot";
 const AddPhotFink = ({ id }) => {
   const dispatch = useDispatch();
   const currentUser = useSelector((state) => state.profile);
+  const photometry = useSelector((state) => state.photometry[id]);
   const permission =
     currentUser.permissions?.includes("System admin") ||
     currentUser.permissions?.includes("Manage sources") ||
     currentUser.permissions?.includes("Upload data");
 
+  const current_magsys = (phot) => {
+    // try to infer the current magsys from the photometry
+    if (phot?.length > 0 && typeof phot[0] === "object") {
+      return phot[0]?.magsys || "ab";
+    }
+    return "ab";
+  };
+
   const handleAddPhotFink = (source_id) => {
-    dispatch(postFinkPhot(source_id))
+    dispatch(postFinkPhot(source_id, current_magsys(photometry)))
       .then((result) => {
         if (result.status === "success") {
           dispatch(showNotification("Fink photometry added"));
@@ -39,12 +48,13 @@ const AddPhotFink = ({ id }) => {
     permission && (
       <Button
         variant="contained"
+        size="small"
         onClick={() => {
           handleAddPhotFink(id);
         }}
-        data-testid="show-photometry-table-button"
+        data-testid="add-phot-fink"
       >
-        Add Phot from Fink
+        Fink Photometry
       </Button>
     )
   );
