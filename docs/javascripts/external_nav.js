7cca0be8fa985e77f667fa71489fc232cd07eb66
Camille DOUZET <camille.douzet@ijclab.in2p3.fr>
2026-09-01 13:27:55 +0200

    removing js stuff , not working

--- docs/javascripts/external_nav.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".md-nav__link[href^='http']").forEach(function (link) {
-    link.setAttribute("target", "_blank");
-    link.setAttribute("rel", "noopener");
-  });
-});
